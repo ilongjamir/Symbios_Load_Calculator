b40cdaf63381295d3ac07177dda9bcb325c73a2c
ilongjamir <116826588+ilongjamir@users.noreply.github.com>
2025-07-12 15:00:20 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("loadcalc-cache").then((cache) =>
+      cache.addAll(["index.html", "manifest.json"])
+    )
+  );
+});
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
